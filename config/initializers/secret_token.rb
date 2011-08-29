a362d05de0dd69f43ca57db664e2d3f781d5eed4
root <root@Dev-USG6.(none)>
2011-08-29 18:50:56 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SocialiteEngage::Application.config.secret_token = '7b4178156439bdd2f6af7c57d648dc166ec321a4d2374dd2979d810b21c0986148edb799b7d569a8f11de195dabaab8c8d2562dd1f018e448113b93884453e57'
