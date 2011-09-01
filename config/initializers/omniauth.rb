d52c3c1e6ab3e2737ee3d9237f47e18c018fb200
Neeraj Kumar <neeraj.kumar@gmail.com>
2011-09-01 18:11:35 +0530

    adding read_streama and manage_apps permissions in omniauth.rb file

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '243721049002445', '5dc3625cf10828498defc160b3e2620d', {:client_options => {:ssl => {:ca_path => "/etc/ssl/certs/"}}, :scope => 'publish_stream,offline_access,email', :access_token=>'243721049002445|b2733d1b4c095ccad1bb0d76.0-100000533980667|0Jf8S3PfwmUWymd97hFvftpqQEA'}
+  provider :facebook, '243721049002445', '5dc3625cf10828498defc160b3e2620d', {:client_options => {:ssl => {:ca_path => "/etc/ssl/certs/"}}, :scope => 'publish_stream,offline_access,email,read_stream,manage_pages', :access_token=>'243721049002445|b2733d1b4c095ccad1bb0d76.0-100000533980667|0Jf8S3PfwmUWymd97hFvftpqQEA'}
 end
 
 
